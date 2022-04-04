64ac726e22f1437542f8c7e046b3833377ea667c
bruno <brunolsantos152@gmail.com>
2022-04-04 09:51:17 -0300

    isso

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,20 +8,11 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
     mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [process.env.privateKey]
-    },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [process.env.privateKey]
+      url: "",
+      accounts: []
     }
-    */
+
   },
   solidity: {
     version: "0.8.4",
